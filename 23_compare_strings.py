56283be76ea1e833f3b10439d13479edfac44a6d
Akash S T <s.t.akash2007@gmail.com>
2026-06-28 19:01:27 +0530

    Implemented string comparison using user input

---
+++ 23_compare_strings.py
@@ -0,0 +1,6 @@
+text1=input("Enter the first string:")
+text2=input("Enter the second string:")
+if text1==text2:
+    print("Both strings are equal")
+else:
+    print("Both strings are not equal")
